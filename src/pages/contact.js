cadc469da1c6532588902d06f3dd3a560494c064
remisaado <remi_saado@hotmail.com>
2022-06-25 15:05:01 +0200

    Fixes "class" instead of "className" mistake.

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Contact = () => {
 
   return (
-    <div class="text-container">
-      <h1 class="subheading">Kontakta oss</h1>
-      <div class="contact-info-box">
+    <div className="text-container">
+      <h1 className="subheading">Kontakta oss</h1>
+      <div className="contact-info-box">
         <ul>
           <li><h4>Kontakt</h4></li>
           <li>Telefon: 031-123456</li>
